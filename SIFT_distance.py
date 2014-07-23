1d839c537b1eb2f6bc3479ae61975937d1e6b4eb
ian harvey <i.c.harvey@cs.cf.ac.uk>
2014-07-23 02:16:43 +0100

    out of memory errors ahoy

--- SIFT_distance.py
+++ SIFT_distance.py
@@ -3,6 +3,7 @@ import cPickle
 import threading
 import cv2
 import numpy as np
+from datetime import time, datetime
 import nltk
 
 __author__ = 'ubuntu'
@@ -72,11 +73,12 @@ class ImageDescriptorManager():
         self.match_threshold = match_threshold
         self.image_descriptors = {}
 
-    def add_descriptor(self, descriptor):
+    def add_descriptor(self, descriptor, add_to_set=True):
         print '\nAdding descriptor ' + str(descriptor.img_idx) + ' ' + descriptor.img_path
 
         for img_idx in self.image_descriptors:
-            print img_idx
+            print str(img_idx) + ' ',
+
             img_desc = self.image_descriptors[img_idx]
 
             sift_key_a, sift_desc_a = descriptor.get_sift(detector=self.detector)
@@ -88,12 +90,30 @@ class ImageDescriptorManager():
                 self.match_threshold
             )
             descriptor.add_distance(img_desc.img_idx, weight)
-            img_desc.add_distance(descriptor.img_idx, weight)
+            if add_to_set:
+                img_desc.add_distance(descriptor.img_idx, weight)
 
-        self.image_descriptors[descriptor.img_idx] = descriptor
+        if add_to_set:
+            self.image_descriptors[descriptor.img_idx] = descriptor
+
+    def get_all_image_signatures(self, weighted=False):
+        img_sigs = dict()
+        for img_desc_idx in self.image_descriptors:
+            img_desc = self.image_descriptors[img_desc_idx]
+            if weighted:
+                img_sigs[img_desc_idx] = img_desc.get_weighted_signature()
+            else:
+                img_sigs[img_desc_idx] = img_desc.get_image_signature()
+        return img_sigs
+
+    def quick_init(self, img_descriptor):
+        self.add_descriptor(img_descriptor, add_to_set=False)
 
 
 def iterall(files_list, match_thresh=1.5):
+
+    time_a = datetime.now()
+    print datetime.now().isoformat()
 
     detector = cv2.SIFT()
     all_descriptors = {}
@@ -103,16 +123,23 @@ def iterall(files_list, match_thresh=1.5):
 
     desc_man = ImageDescriptorManager(match_threshold=match_thresh, detector=detector)
 
+    time_b = datetime.now()
+    print datetime.now().isoformat()
+    time_delta = time_b - time_a
+    print time_delta.total_seconds()
+
+    wrote_sift = 0
     for img_idx in files_list:
         img_path = files_list[img_idx]
         print str(img_idx) + ' : ' + img_path
 
         if os.path.isfile(img_path + '.sift'):
-            print 'found sift file : ' + img_path + '.sift'
-            with open(img_path + '.sift', 'rb') as f:
-                keypoints, descriptors = unpickle_keypoints( cPickle.load(f) )
+            # print 'found sift file : ' + img_path + '.sift'
+            print '.',
+            # with open(img_path + '.sift', 'rb') as f:
+            #     keypoints, descriptors = unpickle_keypoints( cPickle.load(f) )
         else:
-            print 'didnt find sift file : ' + img_path + '.sift'
+            print 'Creating sift : ' + img_path + '.sift'
             img = cv2.imread(img_path)
             img_gray = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY )
             keypoints, descriptors = detector.detectAndCompute(img_gray, None)
@@ -121,84 +148,131 @@ def iterall(files_list, match_thresh=1.5):
 
             with open(img_path + '.sift', 'wb') as f:
                 cPickle.dump(key_desc_temp, f, protocol=cPickle.HIGHEST_PROTOCOL)
-
+            del keypoints
+            del descriptors
+            wrote_sift += 1
                 # all_descriptors[idx] = descriptors
+    print 'had to create ' + str(wrote_sift) + ' .sift files.'
+
+    time_c = datetime.now()
+    print time_c.isoformat()
+    time_delta = time_c - time_b
+    print time_delta.total_seconds()
 
     for filea in files_list:
         img_desc = ImageDescriptor(filea, files_list[filea])
         desc_man.add_descriptor(img_desc)
 
-    print '\n\n*****\n'
+    time_d = datetime.now()
+    print time_d.isoformat()
+    time_delta = time_d - time_c
+    print '*************************************************'
+    print time_delta.total_seconds()
+    print 'Avg ' + str(float(time_delta.total_seconds()) / len(files_list)) + ' secs per img load'
+
     for man_img_idx in desc_man.image_descriptors:
         man_img = desc_man.image_descriptors[man_img_idx]
-        print '\n***\n'
-        print man_img.get_image_signature()
-        print man_img.get_weighted_signature()
+        print '***\n'
+        print 'img sig : ' + man_img.get_image_signature()
+        print 'Image ' + man_img.img_path + ' is most similar to ' + files_list[list(man_img.get_sorted_weights())[0]]
+        # print man_img.get_weighted_signature()
         print '\n'
 
-    # thread_pool = []
+    time_e = datetime.now()
+    print time_e.isoformat()
+    time_delta = time_e - time_d
+    print time_delta.total_seconds()
 
-    for file_a in files_list:
-        print '\nComparing file ' + str(file_a) + ' to : '
+    # for file_a in files_list:
+    #     print '\nComparing file ' + str(file_a) + ' to : '
+    #
+    #     descriptor_orders = {}
+    #     weighted_orders = {}
+    #     with open(files_list[file_a] + '.sift', 'rb') as fa:
+    #         keypoints_a, descriptors_a = unpickle_keypoints( cPickle.load(fa) )
+    #
+    #     for file_b in files_list:
+    #         print file_b,
+    #         if file_a != file_b:
+    #             with open(files_list[file_b] + '.sift', 'rb') as fb:
+    #                 keypoints_b, descriptors_b = unpickle_keypoints( cPickle.load(fb) )
+    #
+    #             matches_size = find_matches(str(file_a) + ',' + str(file_b),
+    #                                         descriptors_a,
+    #                                         descriptors_b,
+    #                                         match_thresh)
+    #             descriptor_orders[str(file_b)] = matches_size
+    #
+    #             weighted_orders[str(file_b)] = (str(file_b) + '=' + str(matches_size)) + ':'
+    #             # weighted_orders[str(file_b)] = (str(file_b) * matches_size)
+    #
+    #             descriptor_distances[str(file_a) + ':' + str(file_b)] = matches_size
+    #
+    #     print '\nImage ' + files_list[file_a] + ' is most similar to ' \
+    #           + files_list[int(list(sorted(descriptor_orders, key=descriptor_orders.get, reverse=True))[0])] + '\n'
+    #
+    #     desc_arr = list(sorted(descriptor_orders, key=descriptor_orders.get, reverse=True))
+    #
+    #     item_string = ''
+    #     for item in desc_arr:
+    #         item_string += str(weighted_orders[item])
+    #     weighted_strings[file_a] = item_string
 
-        descriptor_orders = {}
-        weighted_orders = {}
-        with open(files_list[file_a] + '.sift', 'rb') as fa:
-            keypoints_a, descriptors_a = unpickle_keypoints( cPickle.load(fa) )
+        # desc_strings[file_a] = ' '.join(desc_arr)
+        # print 'Array of img_idx for file ' + str(file_a) + ', closest match first : \n' + desc_strings[file_a]
 
-        for file_b in files_list:
-            print file_b,
-            if file_a != file_b:
-                with open(files_list[file_b] + '.sift', 'rb') as fb:
-                    keypoints_b, descriptors_b = unpickle_keypoints( cPickle.load(fb) )
+    # print '\n****\nArray of distances between images : \n' + str(descriptor_distances)
 
-                matches_size = find_matches(str(file_a) + ',' + str(file_b),
-                                            descriptors_a,
-                                            descriptors_b,
-                                            match_thresh)
-                descriptor_orders[str(file_b)] = matches_size
+    time_f = datetime.now()
+    print time_f.isoformat()
+    time_delta = time_f - time_e
+    print '*********************************************'
+    print time_delta.total_seconds()
 
-                weighted_orders[str(file_b)] = (str(file_b) + '=' + str(matches_size)) + ':'
-                # weighted_orders[str(file_b)] = (str(file_b) * matches_size)
+    # for ws in weighted_strings:
+    #     print str(ws) + ' : ' + weighted_strings[ws]
 
-                descriptor_distances[str(file_a) + ':' + str(file_b)] = matches_size
-
-        print '\nImage ' + files_list[file_a] + ' is most similar to ' \
-              + files_list[int(list(sorted(descriptor_orders, key=descriptor_orders.get, reverse=True))[0])] + '\n'
-
-        desc_arr = list(sorted(descriptor_orders, key=descriptor_orders.get, reverse=True))
-
-        item_string = ''
-        for item in desc_arr:
-            item_string += str(weighted_orders[item])
-        weighted_strings[file_a] = item_string
-
-        desc_strings[file_a] = ' '.join(desc_arr)
-        print 'Array of img_idx for file ' + str(file_a) + ', closest match first : \n' \
-              + desc_strings[file_a]
-
-    print '\n****\nArray of distances between images : \n' + str(descriptor_distances)
-
-    for ws in weighted_strings:
-        print str(ws) + ' : ' + weighted_strings[ws]
+    # print '\n****\nAll images description strings : \n' + str(desc_strings)
+    # for s in desc_strings:
+    #     for st in desc_strings:
+    #         dist = nltk.metrics.edit_distance(
+    #             desc_strings[s],
+    #             desc_strings[st])
+    #         if (len(files_list) * 0.3) < dist < (len(files_list) * 0.8):
+    #             print '\n****\nClosest image pair signatures within 0.8 of each other:'
+    #             print str(s) + ' : ' + desc_strings[s]
+    #             print str(st) + ' : ' + desc_strings[st]
+    #             print 'Deviation of sigs (lower is better) : ' + str(dist)
+    #             print files_list[s]
+    #             print files_list[st]
+    #             print '\n'
 
     print '\n****\nAll images description strings : \n' + str(desc_strings)
-    for s in desc_strings:
-        for st in desc_strings:
+    sigs = desc_man.get_all_image_signatures()
+    for s in sigs:
+        for st in sigs:
             dist = nltk.metrics.edit_distance(
-                desc_strings[s],
-                desc_strings[st])
+                sigs[s],
+                sigs[st])
+            print sigs[s] + '\n' + sigs[st] + '\n' + str(dist)
             if (len(files_list) * 0.3) < dist < (len(files_list) * 0.8):
                 print '\n****\nClosest image pair signatures within 0.8 of each other:'
-                print str(s) + ' : ' + desc_strings[s]
-                print str(st) + ' : ' + desc_strings[st]
+                print str(s) + ' : ' + sigs[s]
+                print str(st) + ' : ' + sigs[st]
                 print 'Deviation of sigs (lower is better) : ' + str(dist)
                 print files_list[s]
                 print files_list[st]
                 print '\n'
 
-                # print str(s) + ' : ' + desc_strings[s]
+    time_g = datetime.now()
+    print time_g.isoformat()
+    time_delta = time_g - time_f
+    print time_delta.total_seconds()
 
+    new_desc = ImageDescriptor(len(files_list), './portrait/11040852736_83cc5c2155_z.jpg')
+    desc_man.quick_init(new_desc)
+    print '\n' + new_desc.get_image_signature()
+    print new_desc.get_weighted_signature()
 
 def pickle_keypoints(keypoints, descriptors):
     i = 0
@@ -294,9 +368,10 @@ def find_files(folders):
         files_dict[int(len(files_dict))] = f
 
     print str(files_dict) + '\n\n****\n'
+    print 'Loading ' + str(len(files_dict)) + ' files.'
     return files_dict
 
 
-to_match = find_files(['./map', './animals', './portrait'])
+to_match = find_files(['./maps', './animals', './portrait_large'])
 
 iterall(to_match, 1.5)
